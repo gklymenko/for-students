f51666f2d2e73ee0f2d381e348666116253c6ef4
Galyna Klymenko <gklymenko@gmail.com>
2023-01-04 11:49:37 +0200

    Added main 5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        //first
-        //added feature 1
-        //added feature 2
-        //added feature 3
-        //added feature 4
+        //added main 5
     }
 
     public static void showLog() {
