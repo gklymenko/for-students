5d6acac47d063f0a919019c77e343546ec5f1c63
Galyna Klymenko <gklymenko@gmail.com>
2023-01-04 12:12:42 +0200

    f1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        //first
-        //added feature 1
-        //added feature 2
-        //added feature 3
-        //added feature 4
-        //added feature 5
+        //From scratch
+        //feature 1
     }
 
     public static void showLog() {
